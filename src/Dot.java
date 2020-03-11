e373db328865a30687698dffd5d6b0f1657933ec
NCollobert <ncollobe@enssat.fr>
2020-03-11 17:08:33 +0100

    Dot class creation

--- src/Dot.java
+++ src/Dot.java
@@ -1,2 +1,27 @@
 public class Dot {
+
+    private int id;
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Point" + id + '(' + x + ';' + y + ')';
+    }
 }
