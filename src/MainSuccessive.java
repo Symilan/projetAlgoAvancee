5e1ee374b9339f4fd0550c3cc4f7cdcd8fbe3d2e
NCollobert <ncollobe@enssat.fr>
2020-04-17 20:31:03 +0100

    Main succesive

--- src/MainSuccessive.java
+++ src/MainSuccessive.java
@@ -1,14 +1,23 @@
 import model.*;
+import visual.Drawing;
 
 public class MainSuccessive {
     public static void main(String[] args) {
         Polygon polygon = new Polygon();
         try {
             polygon.addDot(0,0);
-            polygon.addDot(5,-3);
-            polygon.addDot(10,0);
+            polygon.addDot(2,0);
+            polygon.addDot(3,1);
+            polygon.addDot(5,4);
+            polygon.addDot(4,8);
+            polygon.addDot(2,9);
+            polygon.addDot(1,9);
+            polygon.addDot(-2,3);
+            polygon.addDot(-1,1);
         } catch (Exception e) {
             e.printStackTrace();
         }
+        polygon.triangulationMin();
+        Drawing.draw(polygon);
     }
 }
